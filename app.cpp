09185c8d0d5481529c214d02b294a654603a61f3
IzanamiiDevv <rafaeloli596@gmail.com>
2024-03-21 14:54:25 +0800

    cpp script

---
+++ app.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int hiegth;
+    std::cout << "Insert The Hiegth Of The Piramid:";
+    std::cin >> hiegth;
+    std::cout << hiegth << std::endl;
+    return 0;
+}
